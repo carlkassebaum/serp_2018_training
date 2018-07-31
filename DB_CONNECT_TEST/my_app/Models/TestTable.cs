0e3fae416fafc0fa8f2f3cf3f22cd5cd81d55ef2
carlkassebaum <carlkassebaum@outlook.com>
2018-07-31 22:42:50 +0930

    Adding connection for MySQL and .NET core

---
+++ DB_CONNECT_TEST/my_app/Models/TestTable.cs
@@ -0,0 +1,11 @@
+namespace my_app.Models  
+{  
+    public class TestTable  
+    {  
+        private MusicStoreContext context;  
+  
+        public int id { get; set; }  
+  
+        public string firstname { get; set; }  
+    }  
+} 
