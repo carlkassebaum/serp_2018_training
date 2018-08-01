eb9fa39e4be30f05c77663010b72c6ba9435a067
carlkassebaum <carlkassebaum@outlook.com>
2018-08-02 09:20:44 +0930

    Adding http functionality into angular

--- ANGULAR_TEST/front-end-app/src/app/heroes/heroes.component.ts
+++ ANGULAR_TEST/front-end-app/src/app/heroes/heroes.component.ts
@@ -16,8 +16,25 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  //Called when the corresponding field on the heroes.component.html template is clicked.
+  add(name: string): void 
+  {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
+  }
+
+  //Called when a delete button is pressed. Removes the element from the list and deletes from the server!
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
